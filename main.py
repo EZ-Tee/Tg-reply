9ccb1816cb8f62c8ade81aca148a13a7f81d35e5
Ez-Tee <81809747+EZ-Tee@users.noreply.github.com>
2022-08-19 19:06:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,37 @@ Supun = Client(
     api_hash = os.environ["API_HASH"],
 )
 START_TEXT = """
-හායි {}, I am a image background remover bot. Send me a photo I will send the photo without background.
-Made by [BioHazard Bots](t.me/BioHazard_Bots).
+හායි {}, 
+Made by [Supun](wa.me/+94722908132).
 """
+START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('Updates', url='https://telegram.me/'),
+        InlineKeyboardButton('Support', url='https://telegram.me/')
+        ],[
+        InlineKeyboardButton('Help', callback_data='help'),
+        InlineKeyboardButton('About', callback_data='about'),
+        InlineKeyboardButton('Close', callback_data='close')
+        ]]
+    )
+
+@Supun.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            reply_markup=START_BUTTONS,
+            disable_web_page_preview=True
+        )
+else:
+        await update.message.delete()
+
+
+@Supun.on_message(filters.private & filters.command(["start"]))
+async def start(bot, update):
+    await update.reply_text(
+        text=START_TEXT.format(update.from_user.mention),
+        disable_web_page_preview=True,
+        reply_markup=START_BUTTONS
+    )
+Supun.run()
